da1d8b305daeb42e6329f987a056de38833ce6c3
Sam Revanoor <samrevanoor@Sams-MacBook-Air.local>
2021-04-08 10:30:01 -0400

    one more test for file names

--- Script/app.js
+++ Script/app.js
@@ -61,7 +61,7 @@ $(document).ready(function () {
             points: 30
         }],
         images: [{
-            question: '<h5>Who is this?</h5><img src="jeopardy/Pictures/easy1.gif">',
+            question: '<h5>Who is this?</h5><img src="Pictures/easy1.gif">',
             answer: ["missandei", "misandei", "missandei of narth"],
             points: 10
         }, {
